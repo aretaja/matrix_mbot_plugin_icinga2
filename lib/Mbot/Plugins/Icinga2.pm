c32c98198f8bff01144516a2b30b104d55efcf4b
Marko Punnar <marko@aretaja.org>
2018-03-12 17:58:44 +0200

    Introduce 'hosts down' and 'host <hostname>' commands.

--- lib/Mbot/Plugins/Icinga2.pm
+++ lib/Mbot/Plugins/Icinga2.pm
@@ -26,11 +26,14 @@ sub parse
         my @out = (
             'icinga <command>',
             '  command:',
-            '    status overview - respond with host/service overview',
+            '    status overview - responds with host/service overview',
+            '    hosts down - responds with list of down hosts',
+            '    host <hostname> - responds with detailed host status',
         );
         return join("\n", @out);
     }
 
+    my @out;
     if ($in->{msg} && $in->{msg} =~ m/^icinga\s+(.*)/)
     {
         my $cmd      = $1;
@@ -53,11 +56,7 @@ sub parse
         # Returns status summary
         if ($cmd =~ m/^status\s+overview/)
         {
-            my $result = $icinga->do_request('GET', '/status/CIB',)
-              || die('Icinga2 API fail - '
-                  . $icinga->request_code . ': '
-                  . $icinga->request_status_line);
-
+            my $result = _get($icinga, '/status/CIB');
             $icinga->logout;
 
             my $st;
@@ -70,7 +69,7 @@ sub parse
                 }
             }
 
-            my @out = (
+            @out = (
                 '',
                 'Hosts (up / down / unreach / ack / pending)',
 "  $st->{num_hosts_up} / $st->{num_hosts_down} / $st->{num_hosts_unreachable} / $st->{num_hosts_acknowledged} / $st->{num_hosts_pending}",
@@ -78,14 +77,101 @@ sub parse
 "  $st->{num_services_ok} / $st->{num_services_critical} / $st->{num_services_warning} / $st->{num_services_unknown} / $st->{num_services_unreachable} / $st->{num_services_acknowledged} / $st->{num_services_pending}",
             );
 
-            return join("\n", @out);
+            return (join("\n", @out));
         }
+        elsif ($cmd =~ m/^hosts\s+down/)
+        {
+            my $result = _get($icinga,
+'/objects/hosts?attrs=address&attrs=state&attrs=__name&filter=host.state!=0'
+            );
+            $icinga->logout;
 
+            foreach (@{$result->{results}})
+            {
+                my $name = $_->{attrs}->{__name};
+                my $ip   = $_->{attrs}->{address};
+                push(@out, "$name - $ip");
+            }
+
+            unshift(@out, '') if (@out && scalar(@out) > 1);
+            return (@out) ? join("\n", @out) : "All host are UP";
+        }
+        elsif ($cmd =~ m/^host\s+(.*)\s*/)
+        {
+            my $host = $1;
+            unless ($host =~ m/[\w\.\-]+/)
+            {
+                $icinga->logout;
+                return ("Veird hostname");
+            }
+
+            my $result =
+              _get($icinga, "/objects/hosts?host=$host&meta=used_by");
+
+            if (scalar(@{$result->{results}}) == 1)
+            {
+                my $h = $result->{results}->[0];
+                my $h_stat = ($h->{state} != 0) ? 'DOWN' : 'UP';
+                push(
+                    @out,
+                    (
+                        '',
+"$h->{attrs}->{__name} - $h->{attrs}->{address} - $h_stat",
+                    )
+                );
+                push(@out, "$h->{attrs}->{notes}") if ($h->{attrs}->{notes});
+
+                if ($h->{vars}->{site})
+                {
+                    push(@out, "  Site: $h->{attrs}->{vars}->{site}");
+                    push(@out,
+"    Map: https://maps.google.com/?q=$h->{attrs}->{vars}->{geolocation}"
+                    ) if ($h->{vars}->{geolocation});
+                }
+
+                push(@out, "  Services:");
+
+                foreach (@{$h->{meta}->{used_by}})
+                {
+                    next unless ($_->{type} eq 'Service');
+
+                    $result =
+                      _get($icinga, "/objects/services?service=$_->{name}");
+
+                    if (scalar(@{$result->{results}}) == 1)
+                    {
+                        my $s      = $result->{results}->[0];
+                        my $s_stat = $s->{attrs}->{last_check_result}->{output};
+                        $s_stat =~ s/\n.*//ms;
+                        push(@out,
+                            ("    $s->{attrs}->{name}", "      $s_stat",));
+                    }
+                }
+                return (join("\n", @out));
+            }
+            $icinga->logout;
+        }
         $icinga->logout;
     }
 }
 
 1;
+
+sub _get
+{
+    my ($icinga, $uri) = @_;
+
+    my $r = $icinga->do_request('GET', $uri,);
+    unless ($r)
+    {
+        $icinga->logout;
+        die(    'Icinga2 API fail - '
+              . $icinga->request_code . ': '
+              . $icinga->request_status_line);
+    }
+
+    return ($r);
+}
 
 __END__
 
